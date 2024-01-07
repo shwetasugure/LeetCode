ce11122ad0540a57e88378a2aee055769e1aaf58
Shweta Sugure <107701519+shwetasugure@users.noreply.github.com>
2024-01-07 16:08:11 +0530

    Create Solution.java

---
+++ 1539. Kth Missing Positive Number/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]<=k) k++;
+            else break;
+        }
+        return k;
+    }
+}
