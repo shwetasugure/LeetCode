7353ae9105a43771a763e4592d2f5e0f028894d8
Shweta Sugure <107701519+shwetasugure@users.noreply.github.com>
2024-01-19 22:54:41 +0530

    Create Solution.java

---
+++ 167. Two Sum II - Input Array Is Sorted/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+
+        int l = 0, r = numbers.length-1;
+
+        while(l < r)
+        {
+            int sum = numbers[l] + numbers[r];
+            if(sum == target)
+            {
+                ans[0] = l+1;
+                ans[1] = r+1;
+                break;
+            }
+            else if (sum > target)
+            {
+                r = r - 1;
+            }
+            else
+            {
+               l = l + 1;
+            }
+        }
+        return ans;
+    }
+}
